e36cd82dfbfaad34658d6b6d5d47126aaf8714db
HongJinHyeok <redmax45@naver.com>
2023-05-10 19:00:38 +0900

    new command (install)

---
+++ cmd/install.go
@@ -0,0 +1,84 @@
+package cmd
+
+import (
+	"fmt"
+	"runtime"
+	"strings"
+
+	"github.com/ghdwlsgur/privrew/internal"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var (
+	installCommand = &cobra.Command{
+		Use:   "install",
+		Short: "...",
+		Long:  "...",
+		Run: func(cmd *cobra.Command, args []string) {
+			var (
+				err   error
+				table map[string]internal.Asset
+			)
+
+			// if err = cobra.NoArgs(cmd, args); err != nil {
+			// 	panicRed(err)
+			// }
+			if err = cobra.MinimumNArgs(1)(cmd, args); err != nil {
+				panicRed(err)
+			}
+
+			owner := strings.Split(args[0], "/")[0]
+			repository := strings.Split(args[0], "/")[1]
+			token := viper.GetString("install-token")
+
+			if token == "" {
+				panicRed(fmt.Errorf("..."))
+			}
+
+			repo := &internal.Repository{
+				Name:     repository,
+				Owner:    owner,
+				Token:    token,
+				LocalDir: installDir,
+			}
+
+			os := &internal.OS{
+				Name: runtime.GOOS,
+				Arch: runtime.GOARCH,
+			}
+
+			table, err = internal.GetReleaseLatest(repo, os)
+			if err != nil {
+				panicRed(err)
+			}
+
+			for key := range table {
+				repo.Version = key
+			}
+
+			path, err := internal.DownloadRelease(table, repo)
+			if err != nil {
+				panicRed(err)
+			}
+
+			destPath := "/opt/homebrew/Cellar/" + repo.GetName() + "/" + repo.GetVersion()
+			err = internal.ExtractTarGz(path, destPath, repo.GetName())
+			if err != nil {
+				panicRed(err)
+			}
+
+			src := "../Cellar/oops/" + repo.GetVersion() + "/bin/" + repo.GetName()
+			dest := "/opt/homebrew/bin/" + repo.GetName()
+			internal.CreateSymbolicLink(src, dest)
+
+		},
+	}
+)
+
+func init() {
+	installCommand.Flags().StringP("token", "t", "", "...")
+	viper.BindPFlag("install-token", installCommand.Flag("token"))
+
+	rootCmd.AddCommand(installCommand)
+}
