81c6163eb69078ab6949eff2bea487f057c4aed9
HongJinHyeok <redmax45@naver.com>
2023-05-10 19:00:46 +0900

    new command (tap)

--- cmd/tap.go
+++ cmd/tap.go
@@ -1,7 +1,12 @@
 package cmd
 
 import (
+	"fmt"
+	"strings"
+
+	"github.com/ghdwlsgur/privrew/internal"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 var (
@@ -9,8 +14,38 @@ var (
 		Use:   "tap",
 		Short: "...",
 		Long:  "...",
-		Run: func(_ *cobra.Command, args []string) {
+		Run: func(cmd *cobra.Command, args []string) {
+			var (
+				err error
+			)
 
+			if err = cobra.MinimumNArgs(1)(cmd, args); err != nil {
+				panicRed(err)
+			}
+
+			owner := strings.Split(args[0], "/")[0]
+			repository := strings.Split(args[0], "/")[1]
+			token := viper.GetString("tap-token")
+
+			if token == "" {
+				panicRed(fmt.Errorf("..."))
+			}
+
+			repo := &internal.Repository{
+				Name:     repository,
+				Owner:    owner,
+				Token:    token,
+				LocalDir: tapDir,
+			}
+
+			internal.CloneReleaseRepo(repo)
 		},
 	}
 )
+
+func init() {
+	tapCommand.Flags().StringP("token", "t", "", "...")
+	viper.BindPFlag("tap-token", tapCommand.Flag("token"))
+
+	rootCmd.AddCommand(tapCommand)
+}
